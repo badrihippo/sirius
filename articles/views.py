8d0a484ef6d0356e29f2430eb4155ddafa28fc0f
Hippo <badrihippo@gmail.com>
2015-04-19 16:27:31 +0530

    Added top fixed navbar and icon, and made icon change according to article section.

--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,12 @@ from articles.models import Article, Series, Section, Tag
 class ArticleDetail(generic.DetailView):
     model = Article
     template_name = 'articles/article.htm'
+    
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetail, self).get_context_data(**kwargs)
+        context['page_icon'] = self.object.section.icon_left
+        context['page_section'] = self.object.section.name
+        return context
 class ArticleList(generic.ListView):
     model = Article
     template_name = 'articles/article_list.htm'
